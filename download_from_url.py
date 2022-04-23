5496747a0565c1948a51c303d13ba84f89bee227
lvdat <datb2012191@student.ctu.edu.vn>
2022-04-23 18:23:18 +0000

    beta branch

--- download_from_url.py
+++ download_from_url.py
@@ -5,4 +5,67 @@
 #
 import requests, configparser, json, sys
 
+# sys func
+def exit(err):
+  sys.exit(err)
 
+# read config file
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+
+# get data from config
+
+USER_AGENT = parser.get('Auth', 'USER_AGENT')
+
+
+if(MAIL == '' or PASSWORD == '' or USER_AGENT == '' or APP_KEY == ''):
+  exit("-> Please fill all Auth fields in config.ini!")
+
+print("-> Connecting to Fshare... ")
+
+# api-endpoint (using Fshare API V2)
+URL = USER_API_URL + "/login"
+header = {"Content-Type": "application/json", "accept": "application/json", "User-Agent": USER_AGENT}
+
+# defining a params dict for the parameters to be sent to the API
+Data = {
+  "user_email": MAIL,
+  "password": PASSWORD,
+  "app_key": APP_KEY
+}
+
+# sending get request and saving the response as response object
+r = requests.post(url = URL, headers= header, json = Data)
+
+# check status from response
+# 200: success
+# 405: wrong password
+sc = r.status_code
+print("Status: ", sc)
+
+if(sc == 405):
+  exit("-> Wrong Password, please edit correct information config")
+if(sc == 406):
+  exit("-> Account not activated")
+if(sc == 409):
+  exit("-> Account is locked login")
+if(sc == 410):
+  exit("-> Account is locked")
+if(sc == 424):
+  exit("-> You entered wrong password 3 times, please enter again after 10 minutes")
+if(sc != 200):
+  exit("-> Unknown error.")
+
+#get data from response
+s1 = json.dumps(r.json())
+d = json.loads(s1)
+token = d["token"]
+ssid = d["session_id"]
+
+parser.set('Login', 'SESSION_ID', ssid)
+parser.set('Login', 'TOKEN', token)
+
+with open('config.ini', 'w') as f:
+  parser.write(f)
+  
+print("-> Done!")
